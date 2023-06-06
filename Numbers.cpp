6d464ae5e5b17dd4468e48a22d77fb5a035fcedf
KhanhDeddo <129365445+KhanhDeddo@users.noreply.github.com>
2023-06-06 13:06:52 +0700

    Update Numbers.cpp

--- Numbers.cpp
+++ Numbers.cpp
@@ -1,28 +1,39 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 class Numbers{
     private:
-        int number;
+        int number,temp,count=0;
+        string str;
         string hundred = "hundred";
         string thousand = " thousand";
-        string lessThan10[10]={
+        string lessThan19[20]={
             "zero","one","two","three","four","five",
-            "six","seven","eight","nine"
+            "six","seven","eight","nine","ten",
+            "eleven","twelve","thirteen","fourteen","fifteen",
+            "sixteen","seventeen","eighteen","nineteen"
         };
-        string a[9]={
-            "ten","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"
+        string a[10]={"","",
+            "twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"
         };
     public:
         Numbers(int number){this->number=number;}
         string intToString(){
-            if(number<10){
-                return lessThan10[number]
+            if(number<19){return lessThan19[number];}
+            while(number!=0){
+                count+=1;
+                if(count==1 && number%10!=0){
+                  str=lessThan19[number%10];
+                }
+                else if(count==2 && number%10!=0){
+                  str=a[number%10]+" "+str;
+                }
+                number/=10;
             }
-            while(number%10!=0){
-                while(number%10!=0){
-
-            };
+          return str;
         }
-
-
 };
+int main(){
+  Numbers a(96);
+  cout<<a.intToString();
+  return 0;
+}
