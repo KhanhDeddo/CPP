bd733dfd611260fa47fbec47cc18627ed2782b2a
KhanhDeddo <129365445+KhanhDeddo@users.noreply.github.com>
2023-06-11 09:45:59 +0700

    Add files via upload

--- cat.cpp
+++ cat.cpp
@@ -2,72 +2,43 @@
 using namespace std;
 class Cat{
     private:
-        static int count;
         string name = " ";
-        int age,weight;
+        int number,age,weight;
+        int sumW=0;
     public:
-        Cat(){count += 1;}
-        Cat(string name): name(name){count += 1;}
-        Cat(string name, int age): name(name), age(age){count += 1;}
-        Cat(string name, int age, int weight): name(name), age(age), weight(weight){count += 1;}
+        Cat(){}
+        Cat(string name, int age, int weight): name(name), age(age), weight(weight){}
         ~Cat(){}
-        const Cat& operator=(const Cat&r);
-        Cat operator+(const Cat &that){
-            Cat c;
-            c.name=this->name+" "+that.name;
-            c.age=this->age+that.age;
-            c.weight=this->weight+that.weight;
-            return c;
-        }
-        Cat operator+(int a){
-            Cat c;
-            c.name=this->name+" con";
-            c.age=this->age+a;
-            c.weight=this->weight;
-            return c;
-        }
-        Cat operator++(int){
-            this->weight+=1;
-            return *this;
-        }
-        Cat &operator++(){
-            this->weight+=1;
-            return *this;
-        }
-        Cat operator--(int){
-            this->weight-=1;
-            return *this;
-        }
-        Cat &operator--(){
-            this->weight-=1;
-            return *this;
-        }
-        void operator==(const Cat &that){
-            if(this->age==that.age){cout<<"Yes\n";}
-            else{cout<<"No\n";}
-        }
-        string getName(){return name;}
-        int getAge(){return age;}
-        int getWeight(){return weight;}
-        int getCount(){return count;}
+    friend istream &operator>>(istream &in,Cat &c){
+        cout<<"nhap ten: ";in>>c.name;
+        cout<<"nhap tuoi: ";in>>c.age;
+        cout<<"nhap can nang: ";in>>c.weight;
+        return in;
+    }
+    friend ostream &operator<<(ostream &out,Cat &c){
+        cout<<"ten: ";out<<c.name<<endl;
+        cout<<"tuoi: ";out<<c.age<<endl;
+        cout<<"can nang: ";out<<c.weight<<endl;
+        cout<<"tong can nang: ";out<<c.sumW<<endl;
+        return out;
+    }
 };
-int Cat::count = 0;
-
 int main(){
     system("cls");
-    Cat c1("Tom",1,2),c2("Bibi",2,3);
-    Cat c3=c1;
-    cout <<"So con meo dc tao: "<<c1.getCount()<<endl;
-    cout<<c1.getName()<<" "<<c1.getAge()<<" "<<(c1).getWeight()<<endl;
-    cout<<c2.getName()<<" "<<c2.getAge()<<" "<<(c2).getWeight()<<endl;
-    cout<<(c1+c2).getName()<<" "<<(c1+c2).getAge()<<" "<<(c1+c2).getWeight()<<endl;
-    cout<<(c1+10).getName()<<" "<<(c1+10).getAge()<<" "<<(c1+10).getWeight()<<endl;
-    cout<<(c2++).getWeight()<<endl;
-    cout<<(++c2).getWeight()<<endl;
-    cout<<(c2--).getWeight()<<endl;
-    cout<<(--c2).getWeight()<<endl;
-    c1==c2;
-    c2==c1+1;
-    c1==c3;
+    string name;
+    int n,age,weight;
+    cout<<"nhap sluong meo: ";
+    cin>>n;
+    Cat Cat[n]{},c;
+    for(int i=0;i<n;i++){
+        system("cls");
+        cout<<"nhap thong tin meo thu "<<i+1<<endl;
+        cin>>Cat[i];
+    }
+    system("cls");
+    cout<<"so sluong con meo: "<<n<<endl;
+    for(int i=0;i<n;i++){
+        cout<<Cat[i]<<endl;
+    }
     return 0;
 }
