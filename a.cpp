20fae3b6923113fc29f6e5dca47711745dbc7347
KhanhDeddo <129365445+KhanhDeddo@users.noreply.github.com>
2023-06-18 17:24:53 +0700

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -2,45 +2,78 @@
 using namespace std;
 class Employee{
   private:
-    string name;
-    string maSo;
-    int ngayThue;
+    string name,code_number;
+    int ngay_thue;
   public:
-      Employee(string name, string maSo, int ngayThue ){
+      Employee(string name, string code_number, int ngay_thue ){
           this->name = name;
-          this->maSo = maSo;
-          this->ngayThue = ngayThue;
+          this->code_number = code_number;
+          this->ngay_thue = ngay_thue;
       }
       void setName(string name){this->name = name;}
+      void setCodeNumber(string code_number){this->code_number = code_number;}
+      void setNgayThue(int ngay_thue){this->ngay_thue=ngay_thue;}
       string getName(){return this->name;}
-      void setMaSo(string maSo){this->maSo = maSo;}
-      string getMaSo(){return this->maSo;}
-      void setngayThue(int ngayThue){this->ngayThue=ngayThue;}
-      int getngayThue(){return this->ngayThue;}
+      string getCodeNumber(){return this->code_number;}
+      int getNgayThue(){return this->ngay_thue;}
 };
-
 class ProductionWorker : public Employee{
     private:
-        int caLam,soNgay;
-        float luong,mucLuongtheogio;
-        int check(int cl,float mucltg){
-            float mltg;
-            if(cl==1){mltg=mucltg;}
-            else if(cl==2){mltg=1.5*mucltg;}
-            return mltg;
+        int ca_lam,so_ngay_lam;
+        float luong,luong_theo_gio;
+        void salary(){
+            luong=so_ngay_lam*ca_lam*luong_theo_gio;
         }
-        void salary(){luong=soNgay*caLam*mucLuongtheogio;}
+        int check(int cl,float ltg){
+            float luong_theo_gio;
+            if(cl==1){
+                luong_theo_gio=ltg;
+            }
+            else if(cl==2){
+                luong_theo_gio=1.5*ltg;
+            }
+            return luong_theo_gio;
+        }
     public:
-        ProductionWorker(string name,string maSo,int soNgay,int cl,float mltg ):Employee(name,maSo,soNgay){
-            this->caLam=cl;
-            this->soNgay=soNgay;
-            this->mucLuongtheogio=check(caLam,mltg);
+        ProductionWorker(string name,string code_number,int ngay_thue,int so_ngay_lam,int ca_lam,float luong_theo_gio)
+        :Employee(name,code_number,ngay_thue){
+            this->ca_lam=ca_lam;
+            this->so_ngay_lam=so_ngay_lam;
+            this->luong_theo_gio=check(ca_lam,luong_theo_gio);
             salary();
         }
-        float getLuong(){return luong;}
+        void setCaLam(int ca_lam){
+            this->ca_lam=ca_lam;
+            this->luong_theo_gio=check(ca_lam,luong_theo_gio);
+            salary();
+        }
+        void setLuongTheoGio(float luong_theo_gio){
+            this->luong_theo_gio=check(ca_lam,luong_theo_gio);
+            salary();
+        }
+        void setSoNgayLam(int so_ngay_lam){
+            this->so_ngay_lam=so_ngay_lam;
+            salary();
+        }
+        int getCaLam()const{return ca_lam;}
+        int getSoNgayLam()const{return so_ngay_lam;}
+        float getLuongTheoGio()const{return luong_theo_gio;}
+        float getLuong()const{return luong;}
+        friend ostream& operator<<(ostream &out,ProductionWorker &ps){
+            out<<"Thong tin nhan vien:"<<endl;
+            out<<"Ten: "<<ps.getName()<<endl;
+            out<<"Ma nhan vien: "<<ps.getCodeNumber()<<endl;
+            out<<"Ngay thue: "<<ps.getNgayThue()<<endl;
+            out<<"So ngay lam: "<<ps.getSoNgayLam()<<endl;
+            out<<"Ca lam: "<<ps.getCaLam()<<endl;
+            out<<"Luong theo gio: "<<ps.getLuongTheoGio()<<endl;
+            out<<"Luong theo thang: "<<ps.getLuong()<<endl;
+            return out;
+        }
 };
-int main()
-{  ProductionWorker n1("teo","A14567",14,2,5);
-    cout<<n1.getLuong();
+int main(){  
+    //ProductionWorker p1("Ten","Mã nhan vien","Ngay thue","So ngay lam","Ca làm 1 hoăc 2","Lương theo giờ");
+    ProductionWorker p1("Tao","A45844",1,26,2,5);
+    cout<<p1;
     return 0;
 }
